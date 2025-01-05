a5a0684695d1d29dc8527754cacc3c1bca812a52
jitabhsin <jitabhsin@gmail.com>
2025-01-06 01:36:00 +0530

    symbol X

---
+++ Xpattern.java
@@ -0,0 +1,33 @@
+public class Xpattern {
+    public static void main(String[] args) {
+       
+                int n=5;
+                for(int i=0;i<5;i++){
+                    for(int j=0;j<5;j++){
+                        if(i==j || i+j==n-1){
+                            
+                            System.out.print("x");
+                            }
+                            
+                          
+                          
+                         
+                             
+                          
+                         
+                         
+                         else{
+                             System.out.print(" ");
+                         }
+                        
+                        }
+                        System.out.println();
+                    }
+                 
+                
+                } 
+            
+        }
+        
+    
+
