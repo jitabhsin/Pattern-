4259b2f5d3bb2369fc4ef4f434e8e8f4230074d4
ABHISHEK SINGH <146831587+jitabhsin@users.noreply.github.com>
2025-01-05 15:10:29 +0530

    Update Revpyramid2.java

--- Revpyramid2.java
+++ Revpyramid2.java
@@ -1,1 +1,15 @@
 
+class Revpyramid2 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int row=n;row>=1;row--){
+            for(int col=1;col<=n-row;col++){
+                System.out.print(" ");
+            }
+            for(int col=1;col<=row*2-1;col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
